8fb5efac60b88e4f96e570872d520b7016d37a4f
imiw-corp <imiw.work@gmail.com>
2023-10-29 18:18:38 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,7 +7,6 @@ set USER_PORT=4585
 if not defined PYTHON (set PYTHON=python)
 if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
 
-set pypath=home = %~dp0python
 python\python.exe -m venv venv
 set PYTHON="%VENV_DIR%\Scripts\python.exe"
 
